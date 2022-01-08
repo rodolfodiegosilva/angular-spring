6dd9bbf309ee10aa9bf085241eb8a2536042270f
Rodolfo Silva <rodolfo.diego.gomes@gmail.com>
2022-01-08 16:46:29 -0400

    any commit

--- api/src/main/java/com/diego/api/ApiApplication.java
+++ api/src/main/java/com/diego/api/ApiApplication.java
@@ -1,7 +1,12 @@
 package com.diego.api;
 
+import com.diego.api.model.Course;
+import com.diego.api.repository.CourseRepository;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ApiApplication {
@@ -10,4 +15,14 @@ public class ApiApplication {
 		SpringApplication.run(ApiApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner initDataBase(CourseRepository courseRepository){
+		return args ->{
+			courseRepository.deleteAll();
+			Course c = new Course();
+			c.setName("Angular com Spring");
+			c.setCategory("front-end");
+			courseRepository.save(c);
+		};
+	}
 }
