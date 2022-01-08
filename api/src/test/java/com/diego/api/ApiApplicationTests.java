8d53ba9264203a523bed5bb52d895cea3293f9b7
Rodolfo Silva <rodolfo.diego.gomes@gmail.com>
2022-01-08 13:53:42 -0400

    commit initial spring

---
+++ api/src/test/java/com/diego/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diego.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
